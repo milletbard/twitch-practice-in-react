f680815e94421db636151b90dd5e7704e10f2c0b
Luke Lin <milletbard@gmail.com>
2018-12-30 16:52:25 +0800

    <update>

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = prop => {
   return (
     <div className="footer   text-muted text-center m-4" style={{ padding: 0 }}>
-      This page is created by <a href="https://github.com/cwenwen">me</a> -{" "}
-      <a href="https://github.com/cwenwen/APIsPractice/tree/master/Twitch_API">
+      This page is created by <a href="https://github.com/milletbard">me</a> -{" "}
+      <a href="https://github.com/milletbard/twitch-practice-in-react/tree/master">
         View on Github
       </a>
     </div>
