70c93a8ed9ab809c2a177d9258b32e3384f6a468
Luke Lin <milletbard@gmail.com>
2018-12-30 16:18:36 +0800

    Updates

---
+++ precache-manifest.6ef6c5c096e30c715332510683100541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1ad6c3b67c5a427b139",
+    "url": "/twitch-practice-in-react/static/js/runtime~main.e1ad6c3b.js"
+  },
+  {
+    "revision": "af8b19ce1f5a05143baa",
+    "url": "/twitch-practice-in-react/static/js/main.af8b19ce.chunk.js"
+  },
+  {
+    "revision": "fde70c7ad3d024046d28",
+    "url": "/twitch-practice-in-react/static/js/1.fde70c7a.chunk.js"
+  },
+  {
+    "revision": "af8b19ce1f5a05143baa",
+    "url": "/twitch-practice-in-react/static/css/main.433779a7.chunk.css"
+  },
+  {
+    "revision": "fde70c7ad3d024046d28",
+    "url": "/twitch-practice-in-react/static/css/1.9f984286.chunk.css"
+  },
+  {
+    "revision": "724d52451a0621944dca77586defaff1",
+    "url": "/twitch-practice-in-react/index.html"
+  }
+];
